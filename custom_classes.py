4dfccd5b151c872b5a97d89d282870df17d878c2
Ugurcan <ugurcantomruk2004@hotmail.com>
2026-03-12 23:10:29 +0300

    feat: divide method

--- custom_classes.py
+++ custom_classes.py
@@ -2,4 +2,8 @@ class Calculator(object):
     def __init__(self):
         self._current_val = 0
         
-    
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Can not divide by 0!")
+        self._current_val = x / y
+        return self._current_val
